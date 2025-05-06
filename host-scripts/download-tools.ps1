d49218374a65a33df63104d579f485972db38c8e
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-05-06 14:58:02 -0400

    initial commit

---
+++ host-scripts/download-tools.ps1
@@ -0,0 +1,24 @@
+# download-tools.ps1
+$dest = "C:\data\mycode\sayedha-sandbox\mapped-folder"
+New-Item -ItemType Directory -Path $dest -Force | Out-Null
+
+# Git
+Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.49.0.windows.1/Git-2.49.0-64-bit.exe" -OutFile "$dest\git.exe"
+
+# VS Code
+Invoke-WebRequest -Uri "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64" -OutFile "$dest\vscode.exe"
+
+# .NET SDK 9.0.203
+Invoke-WebRequest -Uri "https://builds.dotnet.microsoft.com/dotnet/Sdk/9.0.203/dotnet-sdk-9.0.203-win-x64.exe" -OutFile "$dest\dotnet9.exe"
+
+# PowerShell 7
+Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.5.1/PowerShell-7.5.1-win-x64.msi" -OutFile "$dest\pwsh.msi"
+
+# Windows Terminal
+# Invoke-WebRequest -Uri "https://github.com/microsoft/terminal/releases/latest/download/Microsoft.WindowsTerminal_1.19.11061.0_8wekyb3d8bbwe.msixbundle" -OutFile "$dest\terminal.msixbundle"
+
+# conemu
+Invoke-WebRequest -Uri "https://github.com/Maximus5/ConEmu/releases/download/v23.07.24/ConEmuSetup.230724.exe" -OutFile "$dest\conemusetup.exe"
+
+# App Installer (winget)
+Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile "$dest\AppInstaller.msixbundle"
---
+++ mapped-folder/install-tools.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal
+
+cd /d C:\setup
+
+rem rem patch the sandbox to prevent long install times more info at https://github.com/microsoft/Windows-Sandbox/issues/68#issuecomment-2684406010
+rem powershell -ExecutionPolicy Bypass -File C:\setup\PatchSandbox.ps1
+
+rem echo Installing App Installer (winget)...
+rem powershell -Command "Add-AppxPackage -Path .\AppInstaller.msixbundle"
+
+echo Installing Git...
+start /wait git.exe /VERYSILENT /NORESTART
+
+echo Installing Visual Studio Code...
+start /wait vscode.exe /VERYSILENT /NORESTART
+
+echo Installing .NET SDK 9.0.203...
+start /wait dotnet9.exe /install /quiet /norestart
+rem start /wait dotnet9.exe /install /norestart
+
+echo Installing PowerShell 7...
+start /wait msiexec /i pwsh.msi /qn
+
+rem echo Installing Windows Terminal...
+rem powershell -Command "Add-AppxPackage -Path .\terminal.msixbundle"
+
+echo Installing ConEmu...
+start /wait ConEmuSetup.exe /p:x64,adm /qr /norestart
+
+echo Setting PowerShell 7 as default...
+set PS7_PATH=%ProgramFiles%\PowerShell\7\pwsh.exe
+if exist "%PS7_PATH%" (
+    del "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk"
+    powershell -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%ProgramData%\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk');$s.TargetPath='%PS7_PATH%';$s.Save()"
+)
+
+echo Enabling 'Show hidden files' and 'Show file extensions'...
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
+
+echo Done!
+pause
