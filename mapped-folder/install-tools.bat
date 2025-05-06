c04d4793974525a4e6644dee4859fcfe671466ab
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-05-06 15:36:29 -0400

    updating install-tools.bat

--- mapped-folder/install-tools.bat
+++ mapped-folder/install-tools.bat
@@ -1,7 +1,9 @@
 @echo off
 setlocal
 
-cd /d C:\setup
+cd /d "%~dp0"
+
+rem cd /d C:\setup
 
 rem rem patch the sandbox to prevent long install times more info at https://github.com/microsoft/Windows-Sandbox/issues/68#issuecomment-2684406010
 rem powershell -ExecutionPolicy Bypass -File C:\setup\PatchSandbox.ps1
@@ -28,6 +30,7 @@ rem powershell -Command "Add-AppxPackage -Path .\terminal.msixbundle"
 echo Installing ConEmu...
 start /wait ConEmuSetup.exe /p:x64,adm /qr /norestart
 
+mkdir "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\"
 echo Setting PowerShell 7 as default...
 set PS7_PATH=%ProgramFiles%\PowerShell\7\pwsh.exe
 if exist "%PS7_PATH%" (
@@ -38,6 +41,16 @@ if exist "%PS7_PATH%" (
 echo Enabling 'Show hidden files' and 'Show file extensions'...
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
+
+
+echo Clone repos
+mkdir c:\data\mycode
+cd c:\data\mycode
+"C:\Program Files\Git\bin\git.exe" clone https://github.com/sayedihashimi/RestaurantService.git
+
+rem add VS code extensions
+"C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-dotnettools.csdevkit --force
+"C:\Program Files\Microsoft VS Code\bin\code" --install-extension GitHub.copilot --force
 
 echo Done!
 pause
